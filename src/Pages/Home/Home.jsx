001e95e3cc150a3c2268842f0e31ca505586bf45
Rashadul Islam <120595628+rashaduldev@users.noreply.github.com>
2023-12-17 15:55:05 +0600

    project setup done

---
+++ src/Pages/Home/Home.jsx
@@ -0,0 +1,12 @@
+import Navber from "../../Components/Navber/Navber";
+
+
+const Home = () => {
+    return (
+        <div>
+            <Navber></Navber>
+        </div>
+    );
+};
+
+export default Home;
